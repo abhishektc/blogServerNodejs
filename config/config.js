fdfb1ec158cb3ee7adba6ede3af355e0f1741232
Abhishek T C <abhishekgowda4258@gmail.com>
2021-02-09 19:38:22 +0530

    Fixing connection string

--- config/config.js
+++ config/config.js
@@ -1,17 +1,22 @@
 const { Pool } = require("pg");
 require("dotenv").config();
 
+// const localConfig = new Pool({
+//     user: process.env.DB_USER,
+//     host: process.env.DB_HOST,
+//     database: process.env.APP_DB,
+//     password: process.env.DB_PASS,
+//     port: process.env.DB_PORT,
+// });
+
 const localConfig = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.APP_DB,
-    password: process.env.DB_PASS,
-    port: process.env.DB_PORT,
+    connectionString: `postgres://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.APP_DB}`
 });
 
-const proConfig = {
-    connectionString: process.env.DATABASE_URL,
-};
+
+const proConfig = new Pool({
+    connectionString: process.env.DATABASE_URL
+});
 
 module.exports = {
     database: process.env.NODE_ENV === "production" ? proConfig : localConfig,
